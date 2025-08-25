def00374ea62bb3c7aa867916883d4d1517fc712
n3rz2008 <daniel.silva.roch2@gmail.com>
2025-08-24 22:42:14 -0300

    json to page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Background from "./components/Background.jsx"
 import Page from "./components/Page.jsx"
+import test from "./layoutTest.json"
 
 function App() {
   return (
     <>
       <Background>
-        <Page/>
+        <Page layout={test}/>
       </Background>
     </>
   )
---
+++ src/components/pageComponents.jsx
@@ -0,0 +1,11 @@
+export function Title({children}) {
+    return(
+        <h1>{children}</h1>
+    )
+}
+
+export function Paragraph({children}) {
+    return(
+        <p>{children}</p>
+    )
+}
