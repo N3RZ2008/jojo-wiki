2b6750cf75b6ab08b62916e2bba6c36a023fc9ad
n3rz2008 <daniel.silva.roch2@gmail.com>
2025-08-25 20:26:02 -0300

    menu added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Background from "./components/Background.jsx"
 import Page from "./components/Page.jsx"
+import Menu from "./components/Menu.jsx"
 import test from "./layoutTest.json"
 
 function App() {
   return (
     <>
       <Background>
+        <Menu/>
         <Page layout={test}/>
       </Background>
     </>
---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import "./styles/menu.css";
+
+function Menu() {
+    return(
+        <div className="menu">
+            <div className="menuItem">Item 1</div>
+            <div className="menuItem">Item 2</div>
+            <div className="menuItem">Item 3</div>
+            <div className="menuItem">Item 4</div>
+            <div className="menuItem">Item 5</div>
+        </div>
+    )
+}
+
+export default Menu
